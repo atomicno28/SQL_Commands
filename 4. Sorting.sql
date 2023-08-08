10224e86cd074209805003019fcf7e3137e1f9be
Nikhil Patel <nikhilid2205@gmail.com>
2023-08-08 18:38:15 +0530

    Important One.

---
+++ 4. Sorting.sql
@@ -0,0 +1,18 @@
+-- 1. sort by ascending
+select name from student order by id;
+
+--2 sort by descending 
+select name from student order DESC by id;
+
+-- sort the string from starting index to n letters.
+select name from student order by Left(name,3);
+
+
+-- sort the string from last index to n letters.
+select name from student order by Right(name,3);
+
+-- if above 2 matches then under such condition
+-- we got to use 'ID' attributes for sort.
+
+select name from student order by Left/Right(name,n),ID;
+
