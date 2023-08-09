224bfb48d3b759d75ccba3dbc32071fc64b5e81b
Nikhil Patel <nikhilid2205@gmail.com>
2023-08-09 16:10:44 +0530

    Searching...

--- 9. REGEX.sql
+++ 9. REGEX.sql
@@ -5,3 +5,18 @@ select city from STATION where city  regexp('^[AEIOU]');
 
 --'[aeiou]$' is a regular expression pattern that matches city names ending with any lowercase vowel (a, e, i, o, or u).
 SELECT CITY FROM STATION WHERE CITY  REGEXP('[aeiou]$');
+
+
+-- not start with vowel
+select city from STATION where city regexp '^[^AEIOU]';
+
+-- NOT END WITH VOWEL
+SELECT CITY FROM STATION WHERE CITY REGEXP '[^aeiou]$';
+
+
+-- .* between
+-- starts with vowel and ends with vowel
+select city from STATION where city regexp '^[aeiou].*[aeiou]$';
+
+--  not starts and ends with vowel.
+select distinct CITY FROM STATION WHERE CITY REGEXP '^[^AEIOU].*[^aeiou]$';
