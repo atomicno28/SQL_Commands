bf3ec2bc846acad07e3954ae91729c70e42e2cab
Nikhil Patel <nikhilid2205@gmail.com>
2023-08-09 14:22:10 +0530

    removal of 'LIKE' keyword

--- 9. REGEX.sql
+++ 9. REGEX.sql
@@ -1,7 +1,7 @@
 -- REGEXP is a keyword used for pattern matching using regular expressions.
 
 --'^[aeiou]' is a regular expression pattern that matches city names starting with any lowercase vowel (a, e, i, o, or u).
-select city from STATION where city like regexp('^[AEIOU]');
+select city from STATION where city  regexp('^[AEIOU]');
 
 --'[aeiou]$' is a regular expression pattern that matches city names ending with any lowercase vowel (a, e, i, o, or u).
-SELECT CITY FROM STATION WHERE CITY LIKE REGEXP('[aeiou]$');
+SELECT CITY FROM STATION WHERE CITY  REGEXP('[aeiou]$');
