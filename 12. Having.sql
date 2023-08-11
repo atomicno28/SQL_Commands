764c474354071e86ff81f868b2de7e4cdcca5561
Nikhil Patel <nikhilid2205@gmail.com>
2023-08-12 01:32:37 +0530

    GroupBy & Having Combined.

--- 12. Having.sql
+++ 12. Having.sql
@@ -1,2 +1,4 @@
 -- GROUP BY HAVING
+-- it will return all the dept. that has entry more than 2.
+
 select Department, count(Department) from Employee group by Department having count(Department) > 2;
