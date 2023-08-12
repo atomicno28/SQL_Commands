e2e4a1cc1cb03a1946f23a79b8cd9cca7d3241f3
Nikhil Patel <nikhilid2205@gmail.com>
2023-08-12 23:43:53 +0530

    group by must be used for HAVING.

--- 12. Having.sql
+++ 12. Having.sql
@@ -2,3 +2,15 @@
 -- it will return all the dept. that has entry more than 2.
 
 select Department, count(Department) from Employee group by Department having count(Department) > 2;
+
+
+-- difference b/w WHERE & HAVING.
+
+-- 1. WHERE clause is used to filter the rows from table based on specific condition
+--   HAVING clause is used to filter the rows from group based on specific condition
+
+-- 2. WHERE is used before GROUPBY clause
+--   HAVING is used after GROUPBY clause.
+
+-- 3.   WHERE may or may not come with group by 
+-- HAVING must come with group by only.
